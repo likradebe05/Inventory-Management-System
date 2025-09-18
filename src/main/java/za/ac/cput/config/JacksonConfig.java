c9d1ab189144e804c10a32875ce3580b940cc63c
mpotsengheisi <mpotsengheisi18@gmail.com>
2025-09-18 18:28:00 +0200

    Updated the Config Files

---
+++ src/main/java/za/ac/cput/config/JacksonConfig.java
@@ -0,0 +1,27 @@
+package za.ac.cput.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        // Register Hibernate6 module to handle lazy loading
+        Hibernate6Module hibernate6Module = new Hibernate6Module();
+        hibernate6Module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
+        hibernate6Module.enable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
+        hibernate6Module.enable(Hibernate6Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
+        
+        mapper.registerModule(hibernate6Module);
+        
+        return mapper;
+    }
+}
