2d0cf6c8c23c83d2c122b00e87d8fc16b34ad71d
mpotsengheisi <mpotsengheisi18@gmail.com>
2025-09-18 18:33:47 +0200

    Updated Factory classes

--- src/main/java/za/ac/cput/Factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/Factory/CustomerFactory.java
@@ -1,28 +1,26 @@
 package za.ac.cput.Factory;
 
-
 import za.ac.cput.Domain.Customer;
+
 public class CustomerFactory {
-    public static Customer createCustomer(String FirstName, String LastName, String email, String password,
+    public static Customer createCustomer(String firstName, String lastName, String email, String password,
                                           String address, String cellphone) {
-        if (FirstName == null || FirstName.isEmpty() || LastName == null || LastName.isEmpty()
-                || email == null || email.isEmpty() || password == null || password.isEmpty()
-                || address == null || address.isEmpty() || cellphone == null || cellphone.isEmpty()) {
+        if (firstName == null || firstName.isEmpty()
+                || lastName == null || lastName.isEmpty()
+                || email == null || email.isEmpty()
+                || password == null || password.isEmpty()
+                || address == null || address.isEmpty()
+                || cellphone == null || cellphone.isEmpty()) {
             return null;
         }
 
-
-
         return new Customer.Builder()
-                .setFirstName(FirstName)
-                .setLastName(LastName)
+                .setFirstName(firstName)
+                .setLastName(lastName)
                 .setEmail(email)
                 .setPassword(password)
                 .setAddress(address)
-                .setCellphone(address)
+                .setCellphone(cellphone)
                 .build();
-
-
-
     }
 }
