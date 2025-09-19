c17e2cb45570d70092cd1b12037bd0c51362a355
likradebe05 <222804424@mycput.ac.za>
2025-09-19 09:04:49 +0200

    The ProductContoller class has been updated

--- src/main/java/za/ac/cput/Controller/ProductController.java
+++ src/main/java/za/ac/cput/Controller/ProductController.java
@@ -2,11 +2,16 @@ package za.ac.cput.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import za.ac.cput.Domain.Product;
 import za.ac.cput.Service.ProductService;
 
 import java.util.List;
 
+/**
+ * Simple ProductController - Legacy endpoints without image handling
+ * Use AdminProductController or InventoryController for full BLOB image support
+ */
 @RestController
 @RequestMapping("/product")
 @CrossOrigin(origins = "http://localhost:3000")
@@ -14,6 +19,11 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping("/getall")
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
 
     @PostMapping("/create")
     public Product create(@RequestBody Product product) {
@@ -34,10 +44,4 @@ public class ProductController {
     public boolean delete(@PathVariable String productId) {
         return productService.delete(productId);
     }
-
-    @GetMapping("/getAll")
-    public List<Product> getAll() {
-        return productService.getAll();
-    }
 }
-
